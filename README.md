72f3ee8017db4953d2cfbda3d88b2d1804274f98
Hailey <haileyacarlson@gmail.com>
2023-03-10 18:16:29 -0800

    added video walkthrough

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # social-network-API
+
+## Description
+An app that you can join and add friends, post thoughts, and add reactions all in one. By using insomnia you can use the correct routes for each option and navigate and use 
+
+## Installation
+Inquirer is needed for this app to work.
+
+## Usage Info
+[Video Walkthrough](https://www.youtube.com/watch?v=GK2xPcTGqdg&ab_channel=HaileyCarlson)
+
+## Contribution
+N/A
+
+## Tests
+N/A
+
+## License
+N/A
--- controllers/userController.js
+++ controllers/userController.js
@@ -29,7 +29,7 @@ module.exports = {
       { runValidators: true, new: true },
     )
       .then((user) =>
-        !video
+        !user
           ? res.status(404).json({ message: 'No user with this id!' })
           : res.json(user),
       )
@@ -55,7 +55,7 @@ module.exports = {
     console.log(req.body)
     User.findOneAndUpdate(
       { _id: req.params.userId },
-      { $addToSet: { friends: req.body } },
+      { $addToSet: { friends: req.params.friendId } },
       { runValidators: true, new: true },
     )
       .then((user) =>
